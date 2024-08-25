af34324684aed2502477135fcdd32990895c76a7
hmkcode <hmkcode@gmail.com>
2024-08-25 21:22:03 +0300

    Create models.js

---
+++ mvc/models.js
@@ -0,0 +1,17 @@
+const models = 
+{
+    formData :
+    {
+        username: 'John Doe',
+        profile: {
+            firstName: 'John',
+            lastName: 'Doe',
+            roles: ['admin', 'editor'],
+            ids: [1, 2]
+        },
+        posts: [
+            { title: 'First Post', content: 'This is the first post' },
+            { title: 'Second Post', content: 'This is the second post' }
+        ],      
+    }
+}
