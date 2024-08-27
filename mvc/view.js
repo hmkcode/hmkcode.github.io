b9fc917c54b30bd094ff0960e867242ffc608bef
hmkcode <hmkcode@gmail.com>
2024-08-27 22:38:32 +0300

    Update view.js

--- mvc/view.js
+++ mvc/view.js
@@ -1,23 +1,45 @@
 fill = (data) => {
     
-    $val('username', data.username);
-    $val('firstName', data.profile.firstName);
-    $val('lastName', data.profile.lastName);
-    $val('roles', data.profile.roles[0]);
-}
-
-collect = () => {
+    $val($e('rqst_by'), data.request.rqst_by);
+    $text($e('rqst_by_r'), data.request.rqst_by);
+    $val($e('vehicle_types'), data.request.vihecle_type);
+    $val($e('vehicle_number'), data.request.vihecle_number);
+   
     
-    const data = {};
-    data.username = $val('username');
-    data.profile = {};
-    data.profile.firstName = $val('firstName');
-    data.profile.lastName = $val('lastName');
-    data.profile.roles = [$val('roles')];
-
 }
 
+
+collect = (data) => {
+    data.rqst_by = $val('rqst_by');
+}
+
+fillTripline = (data) => {
+    $e('tripline').setPoints(data);
+}
+
+fillVehicleTypes = (data) => {
+    $build($e('vehicle_types'), $e('vihecle-type-template'), data, fillVehicleTypesClone);
+}
+
+fillPassengers = (data) => {
+    $build($e('passengers'), $e('passenger-template'), data, fillPassengerClone);
+}
+
+addPassenger = (data) => {
+    $build($e('passengers'), $e('passenger-template'), data, fillPassengerClone);
+}
+
+// ======================
 
 fillRolesClone = (clone, data) => {
     clone.querySelector('li').innerText = data;
+}
+
+fillVehicleTypesClone = (clone, data) => {
+    $text($s('option', clone), data.description);
+    $val($s('option', clone), data.code);    
+}
+
+fillPassengerClone = (clone, data) => {
+    $text($s('h3', clone), data);
 }
